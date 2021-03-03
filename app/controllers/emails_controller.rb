0f87c5554a1a1425263e10242b408b25b7d1e73b
florence <perezflores34000@gmail.com>
2021-03-03 14:21:39 +0100

    ajout exo 1 et 2

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,4 +1,32 @@
 class EmailsController < ApplicationController
   def index
+    @emails = Email.all
+  end
+
+  def create
+    @email = Email.create(email_params)
+  
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
+  def show
+    @email = Email.find(params[:id])
+
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+      
+  end
+  
+  
+  private
+  
+  def email_params
+    params.permit(:object, :body)
   end
 end
+
+
